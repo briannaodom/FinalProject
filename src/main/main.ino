4e40807acdfd2361e85dc7c2ae956d44d779b8b6
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-05-02 00:42:30 -0500

    added code to pring ADC valueto the LCD fro the potientiometer

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,17 +1,21 @@
-const int led = 13;          //pin for led
-const int button = 12;       //pin for button
-boolean prevButton = LOW;    //previous button state
-boolean currentButton = LOW; //current button state
+#include <LiquidCrystal.h>
+
+const int led = 13;             //pin for led
+const int button = 12;          //pin for button
+boolean prevButton = LOW;       //previous button state
+boolean currentButton = LOW;    //current button state
 boolean OFF = false; 
-boolean ledState = OFF;      //state of LED
-int ADCval = 1;              //value for analog reading of POT
-const int POT = A0;          //analog pin value for Potientiometer           
+boolean ledState = OFF;         //state of LED
+int ADCval = 1;                 //value for analog reading of POT
+const int POT = A0;             //analog pin value for Potientiometer   
+LiquidCrystal lcd(7,6,5,4,3,2); //pins arduino in rel.to RS,EN,D4,D5,D6,& D7 on LCD        
 
 boolean debounce(boolean prev);
 
 void setup() {
   pinMode(led, OUTPUT); //led as output, button input by default
   Serial.begin(9600);   //communication speed
+  lcd.begin(16,2);
 }
 
 void loop() {
@@ -30,8 +34,10 @@ void loop() {
   prevButton = currentButton;
   digitalWrite(led, ledState);
   ADCval = readPot(POT);
-  Serial.println(ADCval);
-  delay(450);                     //delay to read ADC values at 450ms (0-1023)
+  Serial.println(ADCval);                            //print ADC values in IDE
+  lcd.setCursor(0,0);
+  lcd.print(String("ADC value: ") + String(ADCval)); //shows ADC values on LCD
+  delay(450);                                        //delay to read ADC values at 450ms (0-1023)
     
 }
 
