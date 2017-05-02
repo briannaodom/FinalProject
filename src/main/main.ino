6c6d89ea623b6ba5ed4ccada6da1ab84896f21a2
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-05-01 19:44:55 -0500

    added code for push button

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,19 +1,23 @@
 const int led = 13; 
+const int button = 2;
 
 void setup() {
-  pinMode(led, OUTPUT);
-  // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT); //led as output, button input by default
 }
 
 void loop() {
-  for (int i = 0; i<256; i++){
-    analogWrite(led, i);
-    delay(10);
+  //for (int i=0; i<256; i++){ //test led to fade
+  //  analogWrite(led, i);
+  //  delay(10);
+  //}
+  //for (int i=255; i>=0; i--){
+  //  analogWrite(led, i);
+  //  delay(10);
+  //}
+    if (digitalRead(button)== HIGH){ //test push button
+      digitalWrite(led, HIGH);
+    }
+    else{
+      digitalWrite(led,LOW);
   }
-  for (int i= 255; i>= 0; i--){
-    analogWrite(led,i);
-    delay(10);
-  }
-  // put your main code here, to run repeatedly:
 }
