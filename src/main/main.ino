b668820b8339b29f42dcdbd400367bacb9ad07e9
Brianna Odom <briannaodom2017@u.northwestern.edu>
2017-05-01 22:50:44 -0500

    added function to potientioeter

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,14 +1,17 @@
-const int led = 13; 
-const int button = 12;
+const int led = 13;          //pin for led
+const int button = 12;       //pin for button
 boolean prevButton = LOW;    //previous button state
 boolean currentButton = LOW; //current button state
 boolean OFF = false; 
 boolean ledState = OFF;      //state of LED
+int ADCval = 1;              //value for analog reading of POT
+const int POT = A0;          //analog pin value for Potientiometer           
 
 boolean debounce(boolean prev);
 
 void setup() {
   pinMode(led, OUTPUT); //led as output, button input by default
+  Serial.begin(9600);   //communication speed
 }
 
 void loop() {
@@ -19,13 +22,17 @@ void loop() {
   //for (int i=255; i>=0; i--){
   //  analogWrite(led, i);
   //  delay(10);
-  //}
-  currentButton = debounce(prevButton);
-    if (prevButton == LOW && currentButton == HIGH){
-      ledState = !ledState;
-    }
-    prevButton = currentButton;
-    digitalWrite(led, ledState);
+  //} 
+  currentButton = debounce(prevButton);             //weird delay in the button
+  if (prevButton == LOW && currentButton == HIGH){
+    ledState = !ledState;
+  }
+  prevButton = currentButton;
+  digitalWrite(led, ledState);
+  ADCval = readPot(POT);
+  Serial.println(ADCval);
+  delay(450);                     //delay to read ADC values at 450ms (0-1023)
+    
 }
 
 
@@ -37,3 +44,9 @@ boolean debounce(boolean previous){
   return currentButtonState;
   }
 }
+
+int readPot(int POT){
+  ADCval = analogRead(POT);
+  return ADCval;
+}
+
